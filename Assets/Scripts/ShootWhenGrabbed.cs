e69c72179ddd454e78639374fdf212ce402f9087
Hong Nam Le <hle02@dcs.bbk.ac.uk>
2019-12-12 14:09:46 +0000

    Added Distance Grab & Trigger Basics

---
+++ Assets/Scripts/ShootWhenGrabbed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+
+public class ShootWhenGrabbed : MonoBehaviour {
+
+    public OVRInput.Button shootButton;
+    private OVRGrabbable ovrGrabbable;
+    private SimpleShoot simpleShoot;
+
+	// Use this for initialization
+	void Start () {
+        simpleShoot = GetComponent<SimpleShoot>();
+        ovrGrabbable = GetComponent<OVRGrabbable>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ovrGrabbable.isGrabbed && OVRInput.GetDown(shootButton, ovrGrabbable.grabbedBy.getController()))
+        {
+            //Shoot
+            simpleShoot.TriggerShoot();
+        }
+	}
+}
